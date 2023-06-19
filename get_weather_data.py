f0bead6a1ac2d46f8e85bc8789585a4fa682519d
Pedro Mussi <phpmussi@gmail.com>
2023-06-19 13:40:10 -0300

    initial commit

---
+++ get_weather_data.py
@@ -0,0 +1,100 @@
+import os
+import requests
+from dotenv import load_dotenv
+import geocoder
+
+geocoding_api_key = os.getenv("GEOCODING_API_KEY")
+
+def get_current_location():
+    g = geocoder.ip('me')
+    return g.city
+
+def get_coordinates(location=None):
+    if location is None:
+        location = get_current_location()
+    
+    api_endpoint = "https://maps.googleapis.com/maps/api/geocode/json"
+    api_key = geocoding_api_key
+
+    parameters = {
+        "address": location,
+        "key": api_key
+    }
+
+    try:
+        response = requests.get(api_endpoint, params=parameters)
+        response.raise_for_status()  
+        geocoding_data = response.json()
+
+        if geocoding_data["status"] == "OK":
+            results = geocoding_data["results"]
+            first_result = results[0]
+            geometry = first_result["geometry"]
+            location = geometry["location"]
+            latitude = location["lat"]
+            longitude = location["lng"]
+
+            return latitude, longitude
+        else:
+            print("Geocoding API error:", geocoding_data["status"])
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return None
+
+def get_uv_radiation(location=None):
+    if location is None:
+        location = get_current_location()
+    
+    latitude, longitude = get_coordinates(location)
+
+    api_endpoint = "https://api.open-meteo.com/v1/forecast"
+    parameters = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "hourly": "uv_index",
+        "daily": "uv_index_max",
+        "forecast_days": 1,
+        "timezone": "auto",
+    }
+
+    try:
+        response = requests.get(api_endpoint, params=parameters)
+        response.raise_for_status()  # Raise an exception if the request was not successful
+        weather_data = response.json()
+
+        daily_uv_index_max = weather_data["daily"]["uv_index_max"]
+        return str(daily_uv_index_max)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return None
+
+def get_rain_precipitation(location=None):
+    if location is None:
+        location = get_current_location()
+    
+    latitude, longitude = get_coordinates(location)
+
+    api_endpoint = "https://api.open-meteo.com/v1/forecast"
+    parameters = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "hourly": "precipitation",
+        "daily": "precipitation_sum",
+        "forecast_days": 1,
+        "timezone": "auto",
+    }
+
+    try:
+        response = requests.get(api_endpoint, params=parameters)
+        response.raise_for_status()  # Raise an exception if the request was not successful
+        weather_data = response.json()
+
+        hourly_precipitation = weather_data["hourly"]["precipitation"]
+        return str(hourly_precipitation)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return None
+
+
+
---
+++ main.py
@@ -0,0 +1,110 @@
+import openai
+import json
+import requests
+import os
+from dotenv import load_dotenv
+from get_weather_data import get_current_location,get_rain_precipitation, get_uv_radiation
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def run_conversation(user_input):
+    # Step 1: send the conversation and available functions to GPT
+    messages = [{"role": "user", "content": user_input}]
+    functions = [
+        #{
+        #    "name": "get_current_location",
+        #    "description": "Get the current location if location wasn't identified in message",
+        #    "parameters": {
+        #        "type": "object",
+        #        "properties": {
+        #            "location": {
+        #                "type": "string",
+        #                "description": "The city, e.g. San Francisco",
+        #            },
+        #        },
+        #    },
+        #},
+        {
+            "name": "get_rain_precipitation",
+            "description": "Get the current precipitation in a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city, e.g. San Francisco",
+                    },
+                },
+                "required": ["location"],
+            },
+        },
+        {
+            "name": "get_uv_radiation",
+            "description": "Get the current uv radiation in a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city, e.g. San Francisco",
+                    },
+                },
+                "required": ["location"],
+            },
+        }
+    ]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        messages=messages,
+        functions=functions,
+        function_call="auto",  # auto is default, but we'll be explicit
+    )
+    response_message = response["choices"][0]["message"]
+
+    # Step 2: check if GPT wanted to call a function
+    if response_message.get("function_call"):
+        # Step 3: call the function
+        # Note: the JSON response may not always be valid; be sure to handle errors
+        available_functions = {
+            "get_rain_precipitation": get_rain_precipitation,
+            "get_uv_radiation": get_uv_radiation
+        }  
+        function_name = response_message["function_call"]["name"]
+        fuction_to_call = available_functions[function_name]
+        function_args = json.loads(response_message["function_call"]["arguments"])
+        function_response = fuction_to_call(
+            location=function_args.get("location")
+        )
+
+        # Step 4: send the info on the function call and function response to GPT
+        messages.append(response_message)  # extend conversation with assistant's reply
+        messages.append(
+            {
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            }
+        )  # extend conversation with function response
+        second_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=messages,
+        )  # get a new response from GPT where it can see the function response
+        return second_response
+
+
+def chat(user_input):
+    response = run_conversation(user_input)
+    return response["choices"][0]["message"]["content"]
+
+# Example usage:
+user_input = input("Type your question: ")
+assistant_response = chat(user_input)
+print(assistant_response)
+
+
+
+
+
+
